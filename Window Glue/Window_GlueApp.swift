7bed762fe0655e9965528d32da7c566dea8fc2a1
conxt <ak@minttype.com>
2025-07-03 12:18:42 +0300

    Initial Commit

---
+++ Window Glue/Window_GlueApp.swift
@@ -0,0 +1,17 @@
+//
+//  Window_GlueApp.swift
+//  Window Glue
+//
+//  Created by Andriy Konstantynov on 03.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct Window_GlueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
